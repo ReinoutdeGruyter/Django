de305f9933a28354f66f3b15be41e2db0219c938
ReinoutdeGruyter <reinoutdegruyter@gmail.com>
2023-12-13 18:58:54 +0100

    Admin.py changed

--- Website/blog/admin.py
+++ Website/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
-
+from .models import Voetbalspeler 
+admin.site.register(Voetbalspeler)
 admin.site.register(Post)
 # Register your models here.
